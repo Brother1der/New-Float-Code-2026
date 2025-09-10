34049c5da86f9e93a9df89d9e4773fa437e7a9a2
Graham <grahamsw10@gmail.com>
2025-09-10 12:25:58 -0500

    Update FloatLogic_V1.py
    Added the basic variables for the use of rfm9x and the float. This might not be complete just yet, so there's probably some errors :(

--- FloatLogic_V1.py
+++ FloatLogic_V1.py
@@ -1,3 +1,4 @@
+#tells people who the authors are! wow :O
 '''
 	Authors: Graham Wunder
 	init create: 9/10/2025
@@ -7,4 +8,38 @@
 import enum from Enum, auto
 import time
 import board
-from digitalio import DigitalInOut, Direction, Pull
+import digitalio
+import busio
+import adafruit_rfm9x
+
+#rfm9x code, currently placeholder
+RADIO_FREQ_MHZ = 915.0
+CS = digitalio.DigitalInOut(board.CE1)
+RESET = digitalio.DigitalInOut(board.D25)
+spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
+rfm9x = adafruit_rfm9x.RFM9x(spi, CS, RESET, RADIO_FREQ_MHZ)
+
+# the radio is configured in LoRa mode so you can't control sync
+# word, encryption, frequency deviation, or other settings
+# you can however adjust the transmit power (in dB). the default is 13 dB but
+# high power radios like the RFM95 can go up to 23 dB
+rfm9x.tx_power = 13
+
+#define the LED
+LED = digitalio.DigitalInOut(board.D13)
+LED.direction = digitalio.Direction.OUTPUT
+
+#create the floatstate enum for tracking the state of the float
+class FloatState(Enum):
+	SURFACED = (auto)
+	MOVING_UP = (auto)
+	MOVING_DOWN = (auto)
+	FLOORED = (auto)
+	MAINTAINING = (auto)
+	IDLE = (auto)
+
+#create the motordirecion enum, self explanatory
+class MotorDirection(Enum):
+	CLOCKWISE = (auto)
+	COUNTERCLOCKWISE = (auto)
+	STALLED = (auto)
