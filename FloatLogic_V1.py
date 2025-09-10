5185beba410f64f222732831f9c557242391c71f
Graham <grahamsw10@gmail.com>
2025-09-10 12:06:05 -0500

    Add files via upload
    Added the start of the first floatlogic file. This may have to be separated at a later date.

---
+++ FloatLogic_V1.py
@@ -0,0 +1,10 @@
+'''
+	Authors: Graham Wunder
+	init create: 9/10/2025
+	last updated: 9/10/2025
+'''
+
+import enum from Enum, auto
+import time
+import board
+from digitalio import DigitalInOut, Direction, Pull
